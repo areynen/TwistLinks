c4496d42c86cdc7c49a2005afe5e053905bd0833
Alex Reynen <alexreynen@gmail.com>
2019-09-09 19:02:11 -0700

    Create check for the video not loading, display an error, and gracefully handle it.

--- main.js
+++ main.js
@@ -8,22 +8,25 @@
 
 (function button_adder() {
     let button = document.createElement('BUTTON');
-    button.onclick = function(){
-        runner();
+    button.onclick = function () {
+        try{
+            runner();
+        } catch (TypeError) {
+            button.textContent = 'Generate Download Links | Please wait for video to load';
+        }
     };
     button.textContent = 'Generate Download Links';
     let video_data = document.querySelector('.video-data');
     video_data.appendChild(button);
 })();
 
-function runner () {
-    document.querySelector('.video-data > button:nth-child(4)').remove();
-    video = document.querySelector('.AT-player > video:nth-child(1)');
-    source = decodeURI(video.src.replace('https://twist.moe', 'https://edge-6.cdn.bunny.sh'));
-    regex = / - (\d\d) /;
-    number = regex.exec(source)[1];
+function runner() {
+    let video = document.querySelector('.AT-player > video:nth-child(1)');
+    let source = decodeURI(video.src.replace('https://twist.moe', 'https://edge-6.cdn.bunny.sh'));
+    let regex = / - (\d\d) /;
+    let number = regex.exec(source)[1];
 
-    div = document.createElement('div');
+    let div = document.createElement('div');
 
     for (let i = 1; i <= 12; i++) {
         let new_link = source.replace(` - ${number} `, ` - ${("0" + i).slice(-2)} `) + '\n';
@@ -35,4 +38,5 @@ function runner () {
 
     let video_data = document.querySelector('.video-data');
     video_data.appendChild(div);
+    document.querySelector('.video-data > button:nth-child(4)').remove();
 }
